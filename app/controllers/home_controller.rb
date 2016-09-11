7dafd2527bf52cbb3b0842e3e112a5e504f31766
Brandon Joyce <bjoyce@covermymeds.com>
2016-09-11 10:10:28 -0400

    Adding AJAX

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,11 @@ class HomeController < ApplicationController
     conn = Faraday.new(url: 'http://uifaces.com')
     response = conn.get('/api/v1/random')
     @avatar_url = JSON.parse(response.body)["image_urls"]["epic"]
+
+    respond_to do |format|
+      format.html { render :index }
+      format.json { render json: { img: @avatar_url } }
+    end
+
   end
 end
